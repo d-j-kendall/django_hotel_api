bd3763710d195167e5957a4037285ee8522a17c6
Dustin Kendall <dkendall100@gmail.com>
2019-10-20 14:48:44 -0500

    Added allowed hosts to settings

--- dm_hotel/settings.py
+++ dm_hotel/settings.py
@@ -29,7 +29,7 @@ with open(os.path.join(BASE_DIR,'secret.txt'), 'r') as f:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hotel.dustinkendall.com']
 
 
 # Application definition
