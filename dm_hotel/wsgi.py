4fdc3d3b696c78bed4132726361b16857c93241c
Dustin Kendall <dkendall100@gmail.com>
2019-10-20 15:20:13 -0500

    Modified wsgi to be compatible with apache

--- dm_hotel/wsgi.py
+++ dm_hotel/wsgi.py
@@ -8,9 +8,17 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
+import time,traceback,signal,sys
 
 from django.core.wsgi import get_wsgi_application
+sys.path.append('/var/www/django_hotel_api')
+sys.path.append('/home/backend/miniconda3/envs/ece354/lib/python3.7')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dm_hotel.settings')
-
-application = get_wsgi_application()
+try:
+    application = get_wsgi_application()
+except Exception:
+    if 'mod_wsgi' in sys.modules:
+        traceback.print_exc()
+        os.kill(os.getpid(), signal.SIGINT)
+        time.sleep(2.5)
