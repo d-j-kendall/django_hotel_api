437aeac5afd759523d13e7665dd9d4d6029d6724
Dustin Kendall <dkendall100@gmail.com>
2019-11-15 18:51:57 -0600

    Added backend, just need to put in the tables and variables in template

--- api/views.py
+++ api/views.py
@@ -26,3 +26,4 @@ def create_user(request):
             
             return Response(serializer.validated_data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
--- home/views.py
+++ home/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import *
+from rest_framework.decorators import api_view
+
+from api.models import *
 import dm_hotel.settings
 from django.template import RequestContext
 
@@ -14,6 +17,16 @@ def create_cust(request):
 def make_reservation(request):
     return render_to_response('home/make_reservation.html')
 
-def check_reservation(request):
-    return render_to_response('templates/check_reservation.html')
 
+@api_view(['GET','POST'])
+def check_reservation(request):
+    reservation = Reservation.objects.get(confirmation_number=request.GET['id'])
+    print(reservation.customer_id)
+    customer = reservation.customer_id
+    room_type = reservation.room_type_id.type
+    res = {
+        'reservation': reservation,
+        'customer' : customer,
+        'room_type': room_type
+    }
+    return render_to_response('templates/check_reservation.html', res)
